e84a4fd191ab02f6f303493bca66a05488c75b03
nadja001 <nadjacibelyfreitas@gmail.com>
2025-08-10 19:42:08 -0300

    Listar e Visualizar(model, listagem e detalhamento)

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Tarefa
 
 # Create your views here.
+
+def listar_tarefas(request):
+    tarefas = Tarefa.objects.all().order_by('-criado_em')
+    return render(request, 'tasks/listar.html', {'tarefas': tarefas})
+
+def detalhes_tarefa(request, pk):
+    tarefa = get_object_or_404(Tarefa, pk=pk)
+    return render(request, 'tasks/detalhes.html', {'tarefa': tarefa})
