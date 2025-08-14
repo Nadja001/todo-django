6ccffce671f157ca1864bc64cf2af7df61b39f61
thamilly11 <camara.maria@escolar.ifrn.edu.br>
2025-08-14 12:57:59 -0300

    implementa criação, edição e exclusão de tarefas

--- tasks/urls.py
+++ tasks/urls.py
@@ -6,4 +6,7 @@ app_name = 'tasks'
 urlpatterns = [
     path('', views.listar_tarefas, name='listar'),
     path('<int:pk>/', views.detalhes_tarefa, name='detalhes'),
+    path('criar/', views.criar_tarefa, name='criar'),
+    path('editar/<int:pk>/', views.editar_tarefa, name='editar'),
+    path('excluir/<int:pk>/', views.excluir_tarefa, name='excluir'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Tarefa
 
 # Create your views here.
@@ -10,3 +10,33 @@ def listar_tarefas(request):
 def detalhes_tarefa(request, pk):
     tarefa = get_object_or_404(Tarefa, pk=pk)
     return render(request, 'tasks/detalhes.html', {'tarefa': tarefa})
+
+def criar_tarefa(request):
+    tarefa = None
+    if request.method == 'POST':
+        titulo = request.POST['titulo']
+        descricao = request.POST['descricao']
+        tarefa = Tarefa(titulo=titulo, descricao=descricao)
+        tarefa.save()
+        return redirect('tasks:listar')
+
+    return render(request, 'tasks/criar.html', {'tarefa': tarefa})
+
+def editar_tarefa(request, pk):
+    tarefa = get_object_or_404(Tarefa, pk=pk)
+    if request.method == 'POST':
+        titulo = request.POST['titulo']
+        descricao = request.POST['descricao']
+        concluida = request.POST['concluida']
+        tarefa.titulo = titulo
+        tarefa.descricao = descricao
+        tarefa.concluida = concluida
+        tarefa.save()
+        return redirect('tasks:listar')
+
+    return render(request, 'tasks/criar.html', {'tarefa': tarefa})
+
+def excluir_tarefa(request, pk):
+    tarefa = get_object_or_404(Tarefa, pk=pk)
+    tarefa.delete()
+    return redirect('tasks:listar')
