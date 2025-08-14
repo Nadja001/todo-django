1cc23e7a2c9548b9d436e737271d04994956ec4d
thamilly11 <camara.maria@escolar.ifrn.edu.br>
2025-08-14 12:33:40 -0300

    melhora o layout dos templates de listar e detalhar tarefas

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'tasks'
+
 urlpatterns = [
     path('', views.listar_tarefas, name='listar'),
     path('<int:pk>/', views.detalhes_tarefa, name='detalhes'),
